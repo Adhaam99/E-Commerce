ad7b49112107eb676b46eae363722b6ba294dbcd
Adhaam99 <adhamyehia194@gmail.com>
2024-08-19 18:26:39 +0300

    intial commit

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductsService } from '../../core/services/products.service';
+import { Product } from '../../core/interfaces/product';
 
 
 @Component({
@@ -9,6 +10,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent  {
- 
+export class ProductsComponent implements OnInit {
+  allProducts: Product[] = [];
+
+  
+
+  constructor(private _ProductsService: ProductsService) {}
+
+  getProduct = () => {
+    this._ProductsService.getProducts().subscribe({
+      next: (res) => {
+        console.log(res.data);
+        this.allProducts = res.data;
+      },
+
+      error: (err) => {
+        console.log(err);
+      },
+
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  };
+
+  ngOnInit(): void {
+    this.getProduct();
+  };
 };
